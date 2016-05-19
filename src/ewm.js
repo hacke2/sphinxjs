29f9f8a4d6f243b0f277f13a2cfac8a176b8fe13
ybbjegj <guomilo@gmail.com>
2016-05-19 14:03:42 +0800

    修复添加二维码，导致在断网时报错以及对获取browserSync host的优化

--- src/ewm.js
+++ src/ewm.js
@@ -1,14 +1,28 @@
 'use strict';
 var qrcode = require('qrcode-terminal');
 var gutil = require('gulp-util');
+var chalk = gutil.colors;
 
-module.exports = function(browserSync) {
-    var opts = browserSync.instance.ui.options.toString(),
-        ip = opts.match(/"ui-external"[^}]*}/)[0].match(/http.*:/)[0],
-        port = opts.match(/"mode": "server"[^}]*}/)[0].match(/\d{4}/)[0],
-        url = ip + port;
+module.exports = function (browserSync) {
+    var localMap = browserSync.instance.options,
+        localUrl;
 
-    qrcode.generate(url, function (qrcode) {
-        gutil.log('\n' + qrcode);
-    });
-}
+    localUrl = localMap.getIn(['urls', 'external']);
+    if (localUrl) {
+        qrcode.generate(localUrl, function (qrcode) {
+            var strs = [];
+
+            strs.push('\n\n');
+
+            strs.push('------------------BrowserSync URLs 二维码-------------------\n\n');
+
+            strs.push(chalk.bold('External:') + chalk.magenta(localUrl + '\n\n'));
+
+            strs.push(qrcode + '\n\n');
+
+            strs.push('--------------------------------------------------------------\n');
+
+            gutil.log(strs.join(''));
+        });
+    }
+};
