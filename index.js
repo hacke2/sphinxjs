6454fb34e2ac8aab6ebd70d273e742d30fcb4c78
maoshuyu <maoshuyu@gmail.com>
2016-07-27 16:50:16 +0800

    refactor: 重构 watch 功能

--- index.js
+++ index.js
@@ -6,80 +6,9 @@ var plugin = require('./src/plugin.js');
 var buildGlob = require('./src/glob.js');
 var config = require('./src/config.js');
 var Store = require('./src/store');
+var watch = require('./src/watch');
 
 var store = new Store;
-
-var running = false;
-var queue = [];
-
-function watch(root, opts, cb) {
-    var delay = 200;
-    var safePathReg = /[\\\/][_\-.\s\w]+$/i;
-
-    function debounce(func, wait) {
-        var timeout, args = [], context, timestamp;
-
-        function later() {
-            // 据上一次触发时间间隔
-            var last = Date.now() - timestamp;
-
-            // 上次被包装函数被调用时间间隔last小于设定时间间隔wait
-            if (last < wait && last > 0) {
-                timeout = setTimeout(later, wait - last);
-            } else {
-                timeout = null;
-                func.apply(context, args);
-                args = [];
-                context = null;
-            }
-        };
-
-        return function () {
-            context = this;
-
-            args = args ? args.concat([].slice.call(arguments)) : [].slice.call(args);
-
-            timestamp = Date.now();
-            // 如果延时不存在，重新设定延时
-            if (!timeout) {
-                timeout = setTimeout(later, wait);
-            }
-        };
-    };
-
-    function listener(type) {
-        return function () {
-            var args = [].slice.call(arguments);
-
-            if (running) {
-                queue.push({
-                    type: type,
-                    args: args
-                });
-
-                return;
-            }
-
-            running = true;
-
-            args.forEach(function (path, i) {
-                if (!safePathReg.test(path)) {
-                    args.splice(i, 1);
-                }
-            });
-
-            if (args.length > 0) {
-                cb(type, args);
-            }
-        };
-    }
-
-    require('chokidar')
-        .watch(root, opts)
-        .on('change', debounce(listener('change'), delay))
-        .on('unlink', debounce(listener('unlink'), delay))
-        .on('add', debounce(listener('add'), delay));
-}
 
 function execute(env) {
     var dest,
@@ -108,11 +37,7 @@ function execute(env) {
             }, store)
             .stream
             .pipe(filter('**/*.css'))
-            .pipe(browserSync.reload({stream: true}))
-            .on('finish', function () {
-                running = false;
-                queue.length = 0;
-            });
+            .pipe(browserSync.reload({stream: true}));
         }
     ]));
 
@@ -134,36 +59,37 @@ function execute(env) {
                         require('path').join(cwd, dest)
                     ],
                     ignoreInitial: true
-                }, function (type, paths) {
-                    var util = require('./src/util.js');
+                }, function (paths) {
                     var cssFlag;
+                    var util = require('./src/util.js');
 
                     if (paths.length === 0) {
                         return;
                     }
 
-                    if (type === 'unlink') {
+                    /* if (type === 'unlink') {
                         paths.forEach(function (path) {
                             store.remove(path);
                         });
                         return;
-                    }
+                    }*/
 
                     cssFlag = paths.every(function (path) {
                         var extname = util.extname(path);
 
                         if (!util.isCss(extname)) {
-                            gulp.series('release', function (cb) {
-                                browserSync.reload();
-                                cb();
-                            })();
                             return false;
                         }
                         return true;
                     });
 
                     if (cssFlag) {
-                        gulp.series('release')();
+                        return gulp.series('release');
+                    } else {
+                        return gulp.series('release', function (cb) {
+                            browserSync.reload();
+                            cb();
+                        });
                     }
                 });
                 cb();
---
+++ src/watch.js
@@ -0,0 +1,86 @@
+'use strict';
+var asyncDone = require('async-done');
+
+function debounce(func, wait) {
+    var timeout, args = [], context, timestamp;
+
+    function later() {
+        // 据上一次触发时间间隔
+        var last = Date.now() - timestamp;
+
+        // 上次被包装函数被调用时间间隔last小于设定时间间隔wait
+        if (last < wait && last > 0) {
+            timeout = setTimeout(later, wait - last);
+        } else {
+            func.apply(context, args);
+            timeout = null;
+            args = [];
+            context = null;
+        }
+    };
+
+    return function () {
+        context = this;
+
+        args = args.concat([].slice.call(arguments));
+
+        timestamp = Date.now();
+        // 如果延时不存在，重新设定延时
+        if (!timeout) {
+            timeout = setTimeout(later, wait);
+        }
+    };
+};
+
+function watch(glob, opts, cb) {
+    var delay = 200;
+    var safePathReg = /[\\\/][_\-.\s\w]+$/i;
+    var running = false;
+    var queue = [];
+    var watcher = require('chokidar').watch(glob, opts);
+
+    function runComplete(err) {
+        running = false;
+        if (err) {
+            watcher.emit('error', err);
+        }
+
+        if (queue.length > 0) {
+            onChange.apply(null, queue);
+            queue.length = 0;
+        }
+    }
+
+    function onChange() {
+        var args = [].slice.call(arguments);
+
+        args.forEach(function (path, i) {
+            if (!safePathReg.test(path)) {
+                args.splice(i, 1);
+            }
+        });
+
+        if (args.length === 0) {
+            return;
+        }
+
+        if (running) {
+            queue = queue.concat(args);
+            return;
+        }
+
+        running = true;
+        asyncDone(cb(args), runComplete);
+    };
+
+    var fn = debounce(onChange, delay);
+
+    watcher
+        .on('change', fn)
+        .on('unlink', fn)
+        .on('add', fn);
+
+    return watcher;
+}
+
+module.exports = watch;
